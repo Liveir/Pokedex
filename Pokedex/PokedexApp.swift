e365b073508784cdb592139f522e95e2dc1023b3
Johnfil Initan <njohnfil.pr+github@gmail.com>
2023-06-29 06:37:10 +0800

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Johnfil Initan on 6/29/23.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
